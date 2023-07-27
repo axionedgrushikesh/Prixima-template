dc2058784e7617d203322e035fc05e2e3ab97ccf
rushikeshgaikwad22 <rushikeshg@axioned.com>
2023-07-27 18:38:48 +0530

    hamberger functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
-/* Author: 
+const hamburger = document.querySelector(".hamburger");
+const headerNav = document.querySelector(".header-nav-links");
 
-*/
+//add active
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  headerNav.classList.toggle("active");
+})
 
-
-
-
-
-
-
-
+//remove active
+navLink.forEach(e => {
+  e.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    headerNav.classList.remove("active");
+  })
+});
 
 
 
