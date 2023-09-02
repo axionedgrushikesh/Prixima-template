eba7b59ca106ef0926911c8094c039264fb1f80e
axionedgrushikesh <rushikeshg@axioned.com>
2023-09-02 15:04:31 +0530

    togling slider dynamically functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,39 @@
+// variable declarations for selectors
 var hamburger = document.querySelector(".hamburger"),
     headerNav = document.querySelector(".header-nav-links"),
-    
-    activeSlide = document.querySelectorAll(".portfolio-slider-toggle li"),
     sliderImages = document.querySelectorAll(".portfolio-slider-list li"),
+    slideBtn = document.querySelectorAll(".slide-btn");
 
-    slideBtn1 = document.querySelector(".slide-btn1"),
-    slideBtn2 = document.querySelector(".slide-btn2"),
-    slideBtn3 = document.querySelector(".slide-btn3"),
-    slideBtn4 = document.querySelector(".slide-btn4"),
-    slideBtn5 = document.querySelector(".slide-btn5");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    headerNav.classList.toggle("active");
-})
-
+// global 
 hamburger.classList.remove("active");
 headerNav.classList.remove("active");
-
-activeSlide.forEach((el) => {
-    el.addEventListener("click", function () {
-        activeSlideRemove();
-        el.classList.add("active");
-    })
-})
 
 let slideIndex = 0;
 showSlides(slideIndex);
 
+// event declaration 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    headerNav.classList.toggle("active");
+});
+
+slideBtn.forEach(el => {
+    el.addEventListener("click", function () {
+        var getUserData = el.getAttribute("data-value");
+        activeSlideRemove();
+        el.classList.add("active");
+        currentSlide(getUserData)
+    })
+})
+
+// finction declarations
 function currentSlide(n) {
     showSlides(slideIndex = n);
+}
+
+function activeSlideRemove() {
+    var activeSlide = document.querySelector(".active");
+    activeSlide.classList.remove("active");
 }
 
 function showSlides() {
@@ -37,29 +41,4 @@ function showSlides() {
     for (i = 0; i < slides.length; i++) {
         a = slides[i].style.transform = `translateX(${slideIndex * 100}%)`
     }
-}
-
-slideBtn1.addEventListener("click", function () {
-    currentSlide(2)
-})
-
-slideBtn2.addEventListener("click", function () {
-    currentSlide(1)
-})
-
-slideBtn3.addEventListener("click", function () {
-    currentSlide(0)
-})
-
-slideBtn4.addEventListener("click", function () {
-    currentSlide(-1)
-})
-
-slideBtn5.addEventListener("click", function () {
-    currentSlide(-2)
-})
-
-function activeSlideRemove() {
-    var activeSlide = document.querySelector(".active");
-    activeSlide.classList.remove("active");
 }
